4abaecd9d5798fd70273a4a8bcd5d94e9579fbc7
TimGITHUB00 <tiitimsan@gmail.com>
2025-09-02 17:50:52 +0300

    Fixed Ore Generation.

--- src/main/java/net/suomiperkele/testmod/TestMod.java
+++ src/main/java/net/suomiperkele/testmod/TestMod.java
@@ -3,8 +3,10 @@ package net.suomiperkele.testmod;
 import net.fabricmc.api.ModInitializer;
 
 import net.suomiperkele.testmod.block.ModBlocks;
+import net.suomiperkele.testmod.component.ModDataComponentTypes;
 import net.suomiperkele.testmod.item.ModItemGroups;
 import net.suomiperkele.testmod.item.ModItems;
+import net.suomiperkele.testmod.world.gen.ModWorldGeneration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,5 +20,8 @@ public class TestMod implements ModInitializer {
 
         ModItems.registerModItems();
         ModBlocks.registerModBlocks();
+
+        ModDataComponentTypes.registerDataComponentTypes();
+        ModWorldGeneration.generateModWorldGen();
 	}
 }
--- src/main/java/net/suomiperkele/testmod/TestModDataGenerator.java
+++ src/main/java/net/suomiperkele/testmod/TestModDataGenerator.java
@@ -2,7 +2,11 @@ package net.suomiperkele.testmod;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.registry.RegistryBuilder;
+import net.minecraft.registry.RegistryKeys;
 import net.suomiperkele.testmod.datagen.*;
+import net.suomiperkele.testmod.world.ModConfiguredFeatures;
+import net.suomiperkele.testmod.world.ModPlacedFeatures;
 
 public class TestModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
@@ -14,6 +18,11 @@ public class TestModDataGenerator implements DataGeneratorEntrypoint {
         pack.addProvider(ModLootTableProvider::new);
         pack.addProvider(ModModelProvider::new);
         pack.addProvider(ModRecipeProvider::new);
-
-	}
+        pack.addProvider(ModRegistryDataGenerator::new);
+    }
+    @Override
+    public void buildRegistry(RegistryBuilder registryBuilder) {
+        registryBuilder.addRegistry(RegistryKeys.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap);
+        registryBuilder.addRegistry(RegistryKeys.PLACED_FEATURE, ModPlacedFeatures::bootstrap);
+    }
 }
